790c8a7b54180a899fbc01c73e35d676802f66b4
DARRYLDARVI <davidfigueroa040726@gmail.com>
2026-02-21 14:33:30 -0600

    feat: cracion del modelo Tarea

---
+++ src/Tarea.java
@@ -0,0 +1,40 @@
+public class Tarea {
+    private int id;
+    private String descripcion;
+    private boolean completada;
+
+    public Tarea(int id, String descripcion, boolean completada) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.completada = completada;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + completada +"]" + id + descripcion;
+    }
+}
