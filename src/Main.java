f48e2a5cd188ab8ab2d46a265818eee49542d53f
DARRYLDARVI <davidfigueroa040726@gmail.com>
2026-02-27 20:00:02 -0600

    feat: implementacion de tratamiento de excepciones

--- src/Main.java
+++ src/Main.java
@@ -24,41 +24,55 @@ public class Main {
             Seleccione la opcion deseada
             """);
 
-            opcion = teclado.nextInt();
+            try{
+                opcion = teclado.nextInt();
 
 
-            switch (opcion){
+                switch (opcion) {
 
-                case 1:
-                    System.out.println("Escriba el id: ");
-                    int id = teclado.nextInt();
-                    teclado.nextLine();
-                    System.out.println("Escriba la descripcion de la tarea: ");
-                    String descripcion = teclado.nextLine();
-                    Tarea tarea = new Tarea(id, descripcion);
-                    repositorio.guardar(tarea);
+                    case 1:
+                        System.out.println("Escriba el id: ");
+                        try {
+                            int id = teclado.nextInt();
+                            teclado.nextLine();
+                            System.out.println("Escriba la descripcion de la tarea: ");
+                            String descripcion = teclado.nextLine();
+                            Tarea tarea = new Tarea(id, descripcion);
+                            repositorio.guardar(tarea);
+                        } catch (java.util.InputMismatchException e){
+                            System.out.println("Error: Ingrese un numero como ID");
+                            teclado.nextLine();
+                        }
 
-                    break;
+                        break;
 
-                case 2:
+                    case 2:
 
-                    for (Tarea t : repositorio.obtenerTodas()){
-                        System.out.println(t);
-                    }
-                    break;
+                        for (Tarea t : repositorio.obtenerTodas()) {
+                            System.out.println(t);
+                        }
+                        break;
 
-                case 3:
-                    System.out.println("Ingrese la tarea que desea buscar por su ID:");
-                    int tareaBuscar = teclado.nextInt();
-                    boolean exito = repositorio.completar(tareaBuscar);
+                    case 3:
+                        System.out.println("Ingrese la tarea que desea buscar por su ID:");
+                        try{
+                            int tareaBuscar = teclado.nextInt();
+                            boolean exito = repositorio.completar(tareaBuscar);
 
-                    if (exito){
-                        System.out.println("Tarea completada!");
-                    }else {
-                        System.out.println("ERROR: No existe ninguna tarea con ese ID");
-                    }
-                    break;
-
+                            if (exito) {
+                                System.out.println("Tarea completada!");
+                            } else {
+                                System.out.println("ERROR: No existe ninguna tarea con ese ID");
+                            }
+                        }catch (java.util.InputMismatchException e){
+                            System.out.println("Error: Ingrese un numero en ID");
+                            teclado.nextLine();
+                        }
+                        break;
+                }
+            }catch (java.util.InputMismatchException e){
+                System.out.println("Error: Porfavor ingresa un numero de las opciones");
+                teclado.nextLine();
             }
         }
 
